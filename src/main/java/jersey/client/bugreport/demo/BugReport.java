c08ef066031432557c8364b685e28a1ea645b0cd
Dimitri Tenenbaum <dim.tbaum@gmail.com>
2023-07-07 19:31:50 +0200

    added a PoC for race condition bug report

---
+++ src/main/java/jersey/client/bugreport/demo/BugReport.java
@@ -0,0 +1,96 @@
+package jersey.client.bugreport.demo;
+
+import java.io.InputStream;
+import java.net.URL;
+import java.security.KeyStore;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.KeyManagerFactory;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSession;
+import javax.net.ssl.TrustManagerFactory;
+
+import jakarta.ws.rs.client.Client;
+import jakarta.ws.rs.client.ClientBuilder;
+import jakarta.ws.rs.core.GenericType;
+import jakarta.ws.rs.core.MediaType;
+
+/**
+ * Jersey client seems to be not thread-safe:
+ * When the first GET request is in progress, 
+ * all parallel requests from other Jersey client instances fail 
+ * with SSLHandshakeException: PKIX path building failed. 
+ * 
+ * Once the first GET request is completed, 
+ * all subsequent requests work without error.
+ *
+ */
+public class BugReport
+{
+  // private static int THREAD_NUMBER = 1; // set THREAD_NUMBER > 1 to reproduce an issue
+  private static int THREAD_NUMBER = 10;
+
+  private volatile static int responseCounter = 0;
+
+  private static SSLContext createContext() throws Exception
+  {
+    URL url= BugReport.class.getResource("keystore.jks");
+    KeyStore keyStore = KeyStore.getInstance("JKS");
+    try (InputStream is = url.openStream())
+    {
+      keyStore.load(is, "password".toCharArray());
+    }
+    KeyManagerFactory kmf = KeyManagerFactory.getInstance("SunX509");
+    kmf.init(keyStore, "password".toCharArray());
+    TrustManagerFactory tmf = TrustManagerFactory.getInstance("PKIX");
+    tmf.init(keyStore);
+    SSLContext context = SSLContext.getInstance("TLS");
+    context.init(kmf.getKeyManagers(), tmf.getTrustManagers(), null);
+    return context;
+  }
+
+  public static void main(String[] args) throws Exception
+  {
+    final HttpsServer server = new HttpsServer(createContext());
+    Executors.newFixedThreadPool(1).submit(server);
+
+    // set THREAD_NUMBER > 1 to reproduce an issue
+    ExecutorService executorService2clients = Executors.newFixedThreadPool(THREAD_NUMBER);
+    
+    final ClientBuilder builder = ClientBuilder.newBuilder().sslContext(createContext())
+        .hostnameVerifier(new HostnameVerifier()
+        {
+          public boolean verify(String arg0, SSLSession arg1)
+          {
+            return true;
+          }
+        });
+
+    for (int i = 0; i < 5; i++)
+    {
+      executorService2clients.submit(new Runnable()
+      {
+        @Override
+        public void run()
+        {
+          try
+          {
+            Client client = builder.build();
+            String ret = client.target("https://127.0.0.1:" + server.getPort()).request(MediaType.TEXT_HTML)
+                .get(new GenericType<String>()
+                {});
+            System.out.println(++responseCounter + ". RET: " + ret);
+          }
+          catch (Exception e)
+          {
+            //get following exception here, if THREAD_NUMBER > 1
+            //jakarta.ws.rs.ProcessingException: javax.net.ssl.SSLHandshakeException: PKIX path building failed:
+            e.printStackTrace();
+          }
+        }
+      });
+    }
+  }
+}
---
+++ src/main/java/jersey/client/bugreport/demo/HttpsServer.java
@@ -0,0 +1,73 @@
+package jersey.client.bugreport.demo;
+
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintWriter;
+
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLServerSocket;
+import javax.net.ssl.SSLSocket;
+
+class HttpsServer implements Runnable
+{
+  private final SSLServerSocket sslServerSocket;
+
+  public HttpsServer(SSLContext context) throws Exception
+  {
+    sslServerSocket = (SSLServerSocket) context.getServerSocketFactory().createServerSocket(0);
+  }
+
+  public int getPort()
+  {
+    return sslServerSocket.getLocalPort();
+  }
+
+  @Override
+  public void run()
+  {
+    System.out.printf("Server started on port %d%n", getPort());
+    while (true)
+    {
+      SSLSocket s;
+      try
+      {
+        s = (SSLSocket) sslServerSocket.accept();
+      }
+      catch (IOException e2)
+      {
+        s = null;
+      }
+      final SSLSocket socket = s;
+      new Thread(new Runnable()
+      {
+        public void run()
+        {
+          try
+          {
+            InputStream is = new BufferedInputStream(socket.getInputStream());
+            byte[] data = new byte[2048];
+            int len = is.read(data);
+            if (len <= 0)
+            {
+              throw new IOException("no data received");
+            }
+            System.out.printf("Server received: %s\n", new String(data, 0, len));
+            PrintWriter writer = new PrintWriter(socket.getOutputStream());
+            writer.println("HTTP/1.1 200 OK");
+            writer.println("Content-Type: text/html");
+            writer.println();
+            writer.println("Hello from server!");
+            writer.flush();
+            writer.close();
+            socket.close();
+          }
+          catch (Exception e1)
+          {
+            e1.printStackTrace();
+          }
+        }
+      }).start();
+    }
+  }
+}
